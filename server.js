01066084e48d632e67c8cc96607f53508eef1d71
dantedimon <dantedimon20@ukr.net>
2018-12-31 10:00:29 +0200

    port added to env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+let port = process.env.PORT || 80;
 
 // app.use('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'index.html'))
@@ -9,4 +10,4 @@ const app = express();
 
 app.use('/', express.static('public'));
 
-app.listen(8080);
+app.listen(port);
